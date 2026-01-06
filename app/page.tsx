54ca07eda4b09065827fc138ddba67ca58d0a794
Hrishikesh Shelke <hrishikeshshelke121@gmail.com>
2026-01-06 12:18:06 +0530

    Move app router to src/app to fix Vercel 404

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: 40, color: "white", background: "black" }}>
-      <h1>Sevendot VFX</h1>
-      <p>Transforming Pixels to Emotions</p>
-    </main>
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Sevendot VFX is LIVE 🚀</h1>
+    </div>
+  );
+}
